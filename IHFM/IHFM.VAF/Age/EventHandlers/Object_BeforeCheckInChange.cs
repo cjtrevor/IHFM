b6466d450167a76fe93c4612f234e805a1fc05cd
Christopher Trevor <christopher.trevor@icloud.com>
2021-09-15 16:13:43 +0200

    Update age on edit of resident added

---
+++ IHFM/IHFM.VAF/Age/EventHandlers/Object_BeforeCheckInChange.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MFiles.VAF.Common;
+using MFilesAPI;
+
+namespace IHFM.VAF
+{
+    public partial class VaultApplication
+    {
+        [EventHandler(MFilesAPI.MFEventHandlerType.MFEventHandlerBeforeCheckInChanges, ObjectType = "MFiles.Object.Resident")]
+        public void SetAge(EventHandlerEnvironment env)
+        {
+            AgeCalculationService ageCalculationService = new AgeCalculationService();
+            ageCalculationService.RefreshAge(env.ObjVerEx, Configuration, false);
+        }
+    }
+}
