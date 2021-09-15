ac143237e17cc860ec94477c6d9b775e995149d3
Christopher Trevor <christopher.trevor@icloud.com>
2021-09-15 16:42:39 +0200

    Add TBC items based on daily, weekly, specific day

--- IHFM/IHFM.VAF/Resident/Services/ResidentPropertyService.cs
+++ IHFM/IHFM.VAF/Resident/Services/ResidentPropertyService.cs
@@ -1,4 +1,5 @@
 ﻿using MFiles.VAF.Common;
+using MFiles.VAF.Configuration;
 using MFilesAPI;
 using System;
 using System.Collections.Generic;
@@ -21,9 +22,71 @@ namespace IHFM.VAF
         public List<ObjVer> GetResidentTBCItems(Lookup residentLookup)
         {
             List<ObjVer> objVers = new List<ObjVer>();
+            ObjVerEx resident = new ObjVerEx(_vault, residentLookup);
+            
+            //Get Daily Items
+            objVers.AddRange(GetTBCItems(resident,_configuration.DailyADLLookup));
 
-            ObjVerEx objVerEx = new ObjVerEx(_vault, residentLookup);
-            Lookups tbcItems = objVerEx.GetLookups(_configuration.TBCADLLookup);
+            //Get Weekly Items
+            objVers.AddRange(GetTBCItems(resident, _configuration.WeekdaysADLLookup));
+
+            //Get Specific Day Items
+            objVers.AddRange(GetTBCItems(resident, GetADLAliasForDayOfWeek()));
+            
+            return objVers;
+        }
+
+        private MFIdentifier GetClinicAliasForDayOfWeek()
+        {
+            switch (DateTime.Now.DayOfWeek)
+            {
+                case DayOfWeek.Sunday:
+                    return _configuration.SundayClinicLookup;
+                case DayOfWeek.Monday:
+                    return _configuration.MondayClinicLookup;
+                case DayOfWeek.Tuesday:
+                    return _configuration.TuesdayClinicLookup;
+                case DayOfWeek.Wednesday:
+                    return _configuration.WednesdayClinicLookup;
+                case DayOfWeek.Thursday:
+                    return _configuration.ThursdayClinicLookup;
+                case DayOfWeek.Friday:
+                    return _configuration.FridayClinicLookup;
+                case DayOfWeek.Saturday:
+                    return _configuration.SaturdayClinicLookup;
+                default:
+                    return _configuration.SundayClinicLookup;
+            }
+        }
+
+        private MFIdentifier GetADLAliasForDayOfWeek()
+        {
+            switch (DateTime.Now.DayOfWeek)
+            {
+                case DayOfWeek.Sunday:
+                    return _configuration.SundayADLLookup;
+                case DayOfWeek.Monday:
+                    return _configuration.MondayADLLookup;
+                case DayOfWeek.Tuesday:
+                    return _configuration.TuesdayADLLookup;
+                case DayOfWeek.Wednesday:
+                    return _configuration.WednesdayADLLookup;
+                case DayOfWeek.Thursday:
+                    return _configuration.ThursdayADLLookup;
+                case DayOfWeek.Friday:
+                    return _configuration.FridayADLLookup;
+                case DayOfWeek.Saturday:
+                    return _configuration.SaturdayADLLookup;
+                default:
+                    return _configuration.SundayADLLookup;
+            }
+        }
+
+        private List<ObjVer> GetTBCItems(ObjVerEx resident, MFIdentifier alias)
+        {
+            List<ObjVer> objVers = new List<ObjVer>();
+
+            Lookups tbcItems = resident.GetLookups(alias);
 
             foreach (Lookup item in tbcItems)
             {
@@ -33,17 +96,20 @@ namespace IHFM.VAF
             return objVers;
         }
 
+
         public List<ObjVer> GetResidentTBCClinicItems(Lookup residentLookup)
         {
             List<ObjVer> objVers = new List<ObjVer>();
+            ObjVerEx resident = new ObjVerEx(_vault, residentLookup);
 
-            ObjVerEx objVerEx = new ObjVerEx(_vault, residentLookup);
-            Lookups tbcItems = objVerEx.GetLookups(_configuration.TBCClinicLookup);
+            //Get Daily Items
+            objVers.AddRange(GetTBCItems(resident, _configuration.DailyClinicLookup));
 
-            foreach (Lookup item in tbcItems)
-            {
-                objVers.Add(item.GetAsObjVer());
-            }
+            //Get Weekly Items
+            objVers.AddRange(GetTBCItems(resident, _configuration.WeekdaysClinicLookup));
+
+            //Get Specific Day Items
+            objVers.AddRange(GetTBCItems(resident, GetClinicAliasForDayOfWeek()));
 
             return objVers;
         }
--- IHFM/IHFM.VAF/TimeBasedCare/Configuration/Configuration.cs
+++ IHFM/IHFM.VAF/TimeBasedCare/Configuration/Configuration.cs
@@ -20,6 +20,45 @@ namespace IHFM.VAF
         public MFIdentifier TBCADLLookup = "MFiles.Property.TBCADL";
 
         [MFPropertyDef(Required = true)]
+        public MFIdentifier DailyADLLookup = "MFiles.Property.EveryDayTBCADL";
+        [MFPropertyDef(Required = true)]
+        public MFIdentifier WeekdaysADLLookup = "MFiles.Property.WeekdaysTBCADL";
+        [MFPropertyDef(Required = true)]
+        public MFIdentifier MondayADLLookup = "MFiles.Property.MondayTBCADL";
+        [MFPropertyDef(Required = true)]
+        public MFIdentifier TuesdayADLLookup = "MFiles.Property.TuesdayTBCADL";
+        [MFPropertyDef(Required = true)]
+        public MFIdentifier WednesdayADLLookup = "MFiles.Property.WednesdayTBCADL";
+        [MFPropertyDef(Required = true)]
+        public MFIdentifier ThursdayADLLookup = "MFiles.Property.ThursdayTBCADL";
+        [MFPropertyDef(Required = true)]
+        public MFIdentifier FridayADLLookup = "MFiles.Property.FridayTBCADL";
+        [MFPropertyDef(Required = true)]
+        public MFIdentifier SaturdayADLLookup = "MFiles.Property.SaturdayTBCADL";
+        [MFPropertyDef(Required = true)]
+        public MFIdentifier SundayADLLookup = "MFiles.Property.SundayTBCADL";
+
+        [MFPropertyDef(Required = true)]
+        public MFIdentifier DailyClinicLookup = "MFiles.Property.EveryDayTBCClinic";
+        [MFPropertyDef(Required = true)]
+        public MFIdentifier WeekdaysClinicLookup = "MFiles.Property.WeekdaysTBCClinic";
+        [MFPropertyDef(Required = true)]
+        public MFIdentifier MondayClinicLookup = "MFiles.Property.MondayTBCClinic";
+        [MFPropertyDef(Required = true)]
+        public MFIdentifier TuesdayClinicLookup = "MFiles.Property.TuesdayTBCClinic";
+        [MFPropertyDef(Required = true)]
+        public MFIdentifier WednesdayClinicLookup = "MFiles.Property.WednesdayTBCClinic";
+        [MFPropertyDef(Required = true)]
+        public MFIdentifier ThursdayClinicLookup = "MFiles.Property.ThursdayTBCClinic";
+        [MFPropertyDef(Required = true)]
+        public MFIdentifier FridayClinicLookup = "MFiles.Property.FridayTBCClinic";
+        [MFPropertyDef(Required = true)]
+        public MFIdentifier SaturdayClinicLookup = "MFiles.Property.SaturdayTBCClinic";
+        [MFPropertyDef(Required = true)]
+        public MFIdentifier SundayClinicLookup = "MFiles.Property.SundayTBCClinic";
+
+
+        [MFPropertyDef(Required = true)]
         public MFIdentifier TBCClinicLookup = "MFiles.Property.TBCClinic";
 
         [MFPropertyDef(Required = true)]
