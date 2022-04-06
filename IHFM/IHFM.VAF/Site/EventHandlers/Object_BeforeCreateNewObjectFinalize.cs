3ae4ec0670ae94e2f17cb1e1af8fab0d4ff6b954
Christopher Trevor <christopher.trevor@icloud.com>
2022-04-06 18:32:46 +0200

    Added new classes

--- IHFM/IHFM.VAF/Site/EventHandlers/Object_BeforeCreateNewObjectFinalize.cs
+++ IHFM/IHFM.VAF/Site/EventHandlers/Object_BeforeCreateNewObjectFinalize.cs
@@ -20,7 +20,18 @@ namespace IHFM.VAF
         [EventHandler(MFEventHandlerType.MFEventHandlerBeforeCreateNewObjectFinalize, Class = "MFiles.Class.OtherDocument")]
         [EventHandler(MFEventHandlerType.MFEventHandlerBeforeCreateNewObjectFinalize, Class = "MFiles.Class.Document")]
         [EventHandler(MFEventHandlerType.MFEventHandlerBeforeCreateNewObjectFinalize, Class = "MFiles.Class.MenuWeekly")]
-        
+        [EventHandler(MFEventHandlerType.MFEventHandlerBeforeCreateNewObjectFinalize, Class = "MFiles.Class.EmergencyLightChecklist")]
+        [EventHandler(MFEventHandlerType.MFEventHandlerBeforeCreateNewObjectFinalize, Class = "MFiles.Class.InspectionFacilityCare")]
+        [EventHandler(MFEventHandlerType.MFEventHandlerBeforeCreateNewObjectFinalize, Class = "MFiles.Class.InspectionHealthCare")]
+
+        [EventHandler(MFEventHandlerType.MFEventHandlerBeforeCreateNewObjectFinalize, Class = "MFiles.Class.GuardhouseHSChecklist")]
+        [EventHandler(MFEventHandlerType.MFEventHandlerBeforeCreateNewObjectFinalize, Class = "MFiles.Class.GuardhouseHandover")]
+        [EventHandler(MFEventHandlerType.MFEventHandlerBeforeCreateNewObjectFinalize, Class = "MFiles.Class.HealthcareShiftReport")]
+        [EventHandler(MFEventHandlerType.MFEventHandlerBeforeCreateNewObjectFinalize, Class = "MFiles.Class.PatrolChecklist")]
+        [EventHandler(MFEventHandlerType.MFEventHandlerBeforeCreateNewObjectFinalize, Class = "MFiles.Class.ReceptionShiftReport")]
+        [EventHandler(MFEventHandlerType.MFEventHandlerBeforeCreateNewObjectFinalize, Class = "MFiles.Class.RiskmanChecklist")]
+        [EventHandler(MFEventHandlerType.MFEventHandlerBeforeCreateNewObjectFinalize, Class = "MFiles.Class.RiskmanShiftReport")]
+
         public void SetSiteIDForOtherDocuments(EventHandlerEnvironment env)
         {
             SitePermissionService sitePermissionService = new SitePermissionService(env.Vault, Configuration);
