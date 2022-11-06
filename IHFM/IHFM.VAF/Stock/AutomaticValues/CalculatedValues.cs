0edd5a16dfac3c0b8d6c98d5563c0f5067005330
Christopher Trevor <christopher.trevor@icloud.com>
2022-11-06 11:12:44 +0200

    Add stock site name to Site stock object

---
+++ IHFM/IHFM.VAF/Stock/AutomaticValues/CalculatedValues.cs
@@ -0,0 +1,33 @@
+﻿using MFiles.VAF.Common;
+using MFilesAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IHFM.VAF
+{
+    public partial class VaultApplication
+    {
+        [PropertyCustomValue("MFiles.Property.StockSite")]
+        public TypedValue SetSiteStockSiteName(PropertyEnvironment env)
+        {
+            string siteName = "Not Found";
+            TypedValue calculated = new TypedValue();
+            SiteSearchService searchService = new SiteSearchService(env.Vault, Configuration);
+
+            int siteNumber = -1;
+            Int32.TryParse(env.ObjVerEx.GetProperty(Configuration.VAFSite).GetValueAsLocalizedText(), out siteNumber);
+
+            if (siteNumber > 0)
+            { 
+                ObjVerEx site = searchService.GetSiteByNumber(siteNumber.ToString());
+                siteName = site.GetProperty(MFBuiltInPropertyDef.MFBuiltInPropertyDefNameOrTitle).GetValueAsLocalizedText();
+            }
+            calculated.SetValue(MFDataType.MFDatatypeText, siteName);
+
+            return calculated;
+        }
+    }
+}
