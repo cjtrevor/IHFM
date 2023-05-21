e3c0208deea4288e09cf347beffb2279a16f9dd3
Christopher Trevor <christopher.trevor@icloud.com>
2023-05-21 10:42:11 +0200

    set last updated fields sisters instruction

---
+++ IHFM/IHFM.VAF/SistersInstruction/Configuration/Configuration.cs
@@ -0,0 +1,23 @@
+﻿using MFiles.VAF.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IHFM.VAF
+{
+    public partial class Configuration
+    {
+        //Property Definition
+        [MFPropertyDef]
+        public MFIdentifier SistersInstructionActivity_SistersIntructionDropdown = "MFiles.Property.PreventativeCareInstruction";
+        [MFPropertyDef]
+        public MFIdentifier SistersInstruction_DateLastActioned = "MFiles.Property.DateLastActioned";
+
+        [MFPropertyDef]
+        public MFIdentifier SistersInstruction_TimeLastActioned = "MFiles.Property.TimeLastActioned";
+
+
+    }
+}
---
+++ IHFM/IHFM.VAF/SistersInstruction/EventHandlers/Object_AfterCreateNewObjectFinalize.cs
@@ -0,0 +1,37 @@
+﻿using MFiles.VAF.Common;
+using MFilesAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IHFM.VAF
+{
+    public partial class VaultApplication
+    {
+        [EventHandler(MFEventHandlerType.MFEventHandlerAfterCreateNewObjectFinalize, Class = "MFiles.Class.SistersInstructionActivity")]
+        public void AfterCreateNewSistersInstructionActivityChange(EventHandlerEnvironment env)
+        {
+            SetLastActionedDateForParentRecord(env);
+        }
+
+        private void SetLastActionedDateForParentRecord(EventHandlerEnvironment env)
+        {
+            if (DevelopmentUtility.IsDevMode(env.ObjVerEx,Configuration))
+            {
+                //Get PArent Object
+                Lookup lookupParent = env.ObjVerEx.GetProperty(Configuration.SistersInstructionActivity_SistersIntructionDropdown).TypedValue.GetValueAsLookup();
+
+                ObjVerEx parent = new ObjVerEx(env.Vault, lookupParent);
+
+                //SetDate
+                parent.SetProperty(Configuration.SistersInstruction_DateLastActioned, MFDataType.MFDatatypeDate, DateTime.Now);
+                //SetTime
+                parent.SetProperty(Configuration.SistersInstruction_TimeLastActioned, MFDataType.MFDatatypeTime, DateTime.Now);
+
+                parent.SaveProperties(); 
+            }
+        }
+    }
+}
