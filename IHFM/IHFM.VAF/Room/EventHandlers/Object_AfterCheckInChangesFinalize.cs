e680072272d2195840a795a8274507e2d1c37902
Christopher Trevor <Admin@ZXG72-WIN1>
2023-12-17 11:11:42 +0200

    Update residents on Room tariff change

---
+++ IHFM/IHFM.VAF/Room/EventHandlers/Object_AfterCheckInChangesFinalize.cs
@@ -0,0 +1,30 @@
+﻿using MFiles.VAF.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IHFM.VAF
+{
+    public partial class VaultApplication
+    {
+        [EventHandler(MFilesAPI.MFEventHandlerType.MFEventHandlerAfterCheckInChangesFinalize, Class = "MFiles.Class.Room")]
+        public void AftermakingChangesToARoom(EventHandlerEnvironment env)
+        {
+            ObjVerChanges changes = new ObjVerChanges(env.ObjVerEx);
+
+            foreach (PropertyValueChange change in changes.Changed)
+            {
+                if (change.PropertyDef == Configuration.RoomTariff.ID && change.ChangeType == PropertyValueChangeType.Modified)
+                {
+                    int itemId = env.ObjVerEx.GetLookupID(Configuration.RoomTariff);
+
+                    RoomPropertyService service = new RoomPropertyService(Configuration);
+                    service.UpdateRoomResidentTariff(env.ObjVerEx, itemId, env.Vault);
+                    break;
+                }
+            }
+        }
+    }
+}
--- IHFM/IHFM.VAF/Room/Services/RoomPropertyService.cs
+++ IHFM/IHFM.VAF/Room/Services/RoomPropertyService.cs
@@ -17,5 +17,29 @@ namespace IHFM.VAF
         {
             room.SaveProperty(_configuration.Vacant, MFDataType.MFDatatypeBoolean, Vacant);
         }
+        
+        public void UpdateRoomResidentTariff(ObjVerEx room, int tariff, Vault vault)
+        {
+            ResidentSearchService search = new ResidentSearchService(vault, _configuration);
+            List<ObjVerEx> roomResidents = search.GetResidentByRoom(room.ID);
+            ObjVerEx resident = new ObjVerEx();
+
+            bool roomHasResident = false;
+            foreach (ObjVerEx res in roomResidents)
+            {
+                if (res.HasValue(_configuration.Active) && res.GetProperty(_configuration.Active).GetValue<bool>())
+                {
+                    resident = res;
+                    roomHasResident = true;
+                    break;
+                }
+            }
+
+            if (roomHasResident)
+            {
+                ResidentPropertyService serv = new ResidentPropertyService(vault, _configuration);
+                serv.SetTariff(resident, tariff);
+            }
+        }
     }
 }
