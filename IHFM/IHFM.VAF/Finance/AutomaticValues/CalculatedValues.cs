a6a5db26c503ae79a31dfe62dac861e5cb379aed
Christopher Trevor <Admin@ZXG72-WIN1>
2023-12-17 11:07:08 +0200

    Autocalculate Finance Invoice Total

---
+++ IHFM/IHFM.VAF/Finance/AutomaticValues/CalculatedValues.cs
@@ -0,0 +1,39 @@
+﻿using MFiles.VAF.Common;
+using MFilesAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IHFM.VAF
+{
+    public partial class VaultApplication
+    {
+        [PropertyCustomValue("MFiles.Property.InvoiceTotal", Priority = 100)]
+        public TypedValue SetFinanceInvoiceTotal(PropertyEnvironment env)
+        {
+            TypedValue value = new TypedValue();
+            double tariff;
+            string totalText;
+
+            string tariffText = env.ObjVerEx.GetProperty(Configuration.Finance_Lodgings).GetValueAsLocalizedText();
+
+            if(!Double.TryParse(tariffText, out tariff))
+            {
+                totalText = "Unable to calculate room tariff.";
+            }
+            else
+            {
+                double familyContribution = env.ObjVerEx.GetProperty(Configuration.Finance_FamilyContribution).GetValue<double>();
+                double familyContributionDiscount = env.ObjVerEx.GetProperty(Configuration.Finance_FamilyContributionDiscount).GetValue<double>();
+
+                totalText = (tariff + familyContribution - familyContributionDiscount).ToString();
+            }
+
+            value.SetValue(MFDataType.MFDatatypeText, totalText);
+
+            return value;
+        }
+    }
+}
---
+++ IHFM/IHFM.VAF/Finance/Configuration/Configuration.cs
@@ -0,0 +1,19 @@
+﻿using MFiles.VAF.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IHFM.VAF
+{
+    public partial class Configuration
+    {
+        [MFPropertyDef]
+        public MFIdentifier Finance_Lodgings = "MFiles.Property.RoomTariff";
+        [MFPropertyDef]
+        public MFIdentifier Finance_FamilyContribution = "MFiles.Property.FamilyContribution";
+        [MFPropertyDef]
+        public MFIdentifier Finance_FamilyContributionDiscount = "MFiles.Property.DiscountOnFamilyContribution";
+    }
+}
