9c53b868c0b283861f4545e2086ff8ba27e33c16
Christopher Trevor <Admin@ZXG72-WIN1>
2024-03-24 10:42:28 +0200

    Added LaundryList Funtions

---
+++ IHFM/IHFM.VAF/LaundryList/AutomaticValues/CalculatedValues.cs
@@ -0,0 +1,138 @@
+﻿using MFiles.VAF.Common;
+using MFiles.VAF.Configuration;
+using MFilesAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IHFM.VAF
+{
+    public partial class VaultApplication
+    {
+        [PropertyCustomValue("MFiles.Property.TotalItems", Priority = 100)]
+        public TypedValue SetTotalLaundryItemsValue(PropertyEnvironment env)
+        {
+            TypedValue calculated = new TypedValue();
+
+            LaundryCalculationService service = new LaundryCalculationService();
+
+            List<MFIdentifier> configs = new List<MFIdentifier>
+            {
+                Configuration.Laundry_Baadjies
+                ,Configuration.Laundry_BedBeskermings
+                ,Configuration.Laundry_BedJakkies
+                ,Configuration.Laundry_BedSokkies
+                ,Configuration.Laundry_Bloese
+                ,Configuration.Laundry_Borslappe
+                ,Configuration.Laundry_Bra
+                ,Configuration.Laundry_Comforter
+                ,Configuration.Laundry_DamesFrokkies
+                ,Configuration.Laundry_Dekens
+                ,Configuration.Laundry_DriekwartBroeke
+                ,Configuration.Laundry_Duvet
+                ,Configuration.Laundry_DuvetCovers
+                ,Configuration.Laundry_Gordyne
+                ,Configuration.Laundry_Handdoeke
+                ,Configuration.Laundry_Hemde
+                ,Configuration.Laundry_KamerJasse
+                ,Configuration.Laundry_Keppies
+                ,Configuration.Laundry_Kleedtjies
+                ,Configuration.Laundry_Komberse
+                ,Configuration.Laundry_KortBroeke
+                ,Configuration.Laundry_Kussing
+                ,Configuration.Laundry_Kussingslope
+                ,Configuration.Laundry_Laken
+                ,Configuration.Laundry_LangBroeke
+                ,Configuration.Laundry_LangpypOnderbroeke
+                ,Configuration.Laundry_Leggings
+                ,Configuration.Laundry_MansFrokkies
+                ,Configuration.Laundry_Nagrokke
+                ,Configuration.Laundry_Onderbroeke
+                ,Configuration.Laundry_Oorjasse
+                ,Configuration.Laundry_Oorpakbaadjies
+                ,Configuration.Laundry_OorpakBroeke
+                ,Configuration.Laundry_PajamaTops
+                ,Configuration.Laundry_PajamaBroeke
+                ,Configuration.Laundry_Panties
+                ,Configuration.Laundry_Paslaken
+                ,Configuration.Laundry_Rokke
+                ,Configuration.Laundry_Rompe
+                ,Configuration.Laundry_Sakdoeke
+                ,Configuration.Laundry_Servette
+                ,Configuration.Laundry_Sokkies
+                ,Configuration.Laundry_SweetpakBroeke
+                ,Configuration.Laundry_SweetpakTops
+                ,Configuration.Laundry_Swembroeke
+                ,Configuration.Laundry_Tafeldoeke
+                ,Configuration.Laundry_THemde
+                ,Configuration.Laundry_Treklaken
+                ,Configuration.Laundry_Truie
+                ,Configuration.Laundry_WasgoedSakke
+        };
+
+            calculated.SetValue(MFDataType.MFDatatypeInteger, service.GetScoreSumFromListValues(env.ObjVerEx, configs));
+
+            return calculated;
+        }
+
+
+        [PropertyCustomValue("MFiles.Property.TotalChargableItems", Priority = 100)]
+        public TypedValue SetTotalLaundryTotalChargeableItemsValue(PropertyEnvironment env)
+        {
+            TypedValue calculated = new TypedValue();
+
+            LaundryCalculationService service = new LaundryCalculationService();
+
+            List<MFIdentifier> configs = new List<MFIdentifier>
+            {
+                Configuration.Laundry_Baadjies
+                ,Configuration.Laundry_BedBeskermings
+                ,Configuration.Laundry_BedJakkies
+                ,Configuration.Laundry_BedSokkies
+                ,Configuration.Laundry_Bloese
+                ,Configuration.Laundry_Borslappe
+                ,Configuration.Laundry_Comforter
+                ,Configuration.Laundry_Dekens
+                ,Configuration.Laundry_DriekwartBroeke
+                ,Configuration.Laundry_Duvet
+                ,Configuration.Laundry_DuvetCovers
+                ,Configuration.Laundry_Gordyne
+                ,Configuration.Laundry_Handdoeke
+                ,Configuration.Laundry_Hemde
+                ,Configuration.Laundry_KamerJasse
+                ,Configuration.Laundry_Kleedtjies
+                ,Configuration.Laundry_Komberse
+                ,Configuration.Laundry_KortBroeke
+                ,Configuration.Laundry_Kussing
+                ,Configuration.Laundry_Kussingslope
+                ,Configuration.Laundry_Laken
+                ,Configuration.Laundry_LangBroeke
+                ,Configuration.Laundry_Leggings
+                ,Configuration.Laundry_Nagrokke
+                ,Configuration.Laundry_Oorjasse
+                ,Configuration.Laundry_Oorpakbaadjies
+                ,Configuration.Laundry_OorpakBroeke
+                ,Configuration.Laundry_PajamaTops
+                ,Configuration.Laundry_PajamaBroeke
+                ,Configuration.Laundry_Paslaken
+                ,Configuration.Laundry_Rokke
+                ,Configuration.Laundry_Rompe
+                ,Configuration.Laundry_SweetpakBroeke
+                ,Configuration.Laundry_SweetpakTops
+                ,Configuration.Laundry_Swembroeke
+                ,Configuration.Laundry_Tafeldoeke
+                ,Configuration.Laundry_THemde
+                ,Configuration.Laundry_Treklaken
+                ,Configuration.Laundry_Truie
+                ,Configuration.Laundry_WasgoedSakke
+        };
+
+            calculated.SetValue(MFDataType.MFDatatypeInteger, service.GetScoreSumFromListValues(env.ObjVerEx, configs));
+
+            return calculated;
+        }
+    }
+}
+
---
+++ IHFM/IHFM.VAF/LaundryList/Configuration/Configuration.cs
@@ -0,0 +1,63 @@
+﻿using MFiles.VAF.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IHFM.VAF
+{
+    public partial class Configuration
+    {
+        public MFIdentifier Laundry_Baadjies = "PD.Jacket";
+        public MFIdentifier Laundry_BedBeskermings = "MFiles.Property.BedBeskermingsProtector";
+        public MFIdentifier Laundry_BedJakkies = "MFiles.Property.Bedsokkies";
+        public MFIdentifier Laundry_BedSokkies = "MFiles.Property.Bedsokkies";
+        public MFIdentifier Laundry_Bloese = "PD.Blouse";
+        public MFIdentifier Laundry_Borslappe = "MFiles.Property.Borslappe";
+        public MFIdentifier Laundry_Bra = "PD.Bra";
+        public MFIdentifier Laundry_Comforter = "PD.Comforter";
+        public MFIdentifier Laundry_DamesFrokkies = "MFiles.Property.DamesFrokkies";
+        public MFIdentifier Laundry_Dekens = "MFiles.Property.Dekens";
+        public MFIdentifier Laundry_DriekwartBroeke = "MFiles.Property.DriekwartBroekeClothing";
+        public MFIdentifier Laundry_Duvet = "MFiles.Property.Duvet";
+        public MFIdentifier Laundry_DuvetCovers = "MFiles.Property.DuvetCovers";
+        public MFIdentifier Laundry_Gordyne = "MFiles.Property.Gordynecurtains";
+        public MFIdentifier Laundry_Handdoeke = "PD.Towels";
+        public MFIdentifier Laundry_Hemde = "PD.ShirtClothes";
+        public MFIdentifier Laundry_KamerJasse = "PD.Gown";
+        public MFIdentifier Laundry_Keppies = "PD.Cap";
+        public MFIdentifier Laundry_Kleedtjies = "MFiles.Property.Kleedtjieslappies";
+        public MFIdentifier Laundry_Komberse = "PD.Blanket";
+        public MFIdentifier Laundry_KortBroeke = "MFiles.Property.KortBroekeshortPantsClothing";
+        public MFIdentifier Laundry_Kussing = "PD.Pillow";
+        public MFIdentifier Laundry_Kussingslope = "PD.PillowCase";
+        public MFIdentifier Laundry_Laken = "MFiles.Property.Lakensheet";
+        public MFIdentifier Laundry_LangBroeke = "PD.PantsClothing";
+        public MFIdentifier Laundry_LangpypOnderbroeke = "MFiles.Property.LangpypOnderbroeke";
+        public MFIdentifier Laundry_Leggings = "MFiles.Property.Lefggingsskipantstights";
+        public MFIdentifier Laundry_MansFrokkies = "MFiles.Property.MansFrokkies";
+        public MFIdentifier Laundry_Nagrokke = "PD.Nighty";
+        public MFIdentifier Laundry_Onderbroeke = "PD.Underpants";
+        public MFIdentifier Laundry_Oorjasse = "MFiles.Property.Oorjasse";
+        public MFIdentifier Laundry_Oorpakbaadjies = "MFiles.Property.Oorpakbaadtjies";
+        public MFIdentifier Laundry_OorpakBroeke = "MFiles.Property.Oorpakbroeke";
+        public MFIdentifier Laundry_PajamaTops = "MFiles.Property.PajamaBaadjiestops";
+        public MFIdentifier Laundry_PajamaBroeke = "MFiles.Property.PajamaBroekepants";
+        public MFIdentifier Laundry_Panties = "PD.Panty";
+        public MFIdentifier Laundry_Paslaken = "PD.FittedSheet";
+        public MFIdentifier Laundry_Rokke = "MFiles.Property.Dress";
+        public MFIdentifier Laundry_Rompe = "PD.Skirt";
+        public MFIdentifier Laundry_Sakdoeke = "PD.Handkerchiefs";
+        public MFIdentifier Laundry_Servette = "MFiles.Property.Servette";
+        public MFIdentifier Laundry_Sokkies = "MFiles.Property.SokkiesEnkelstukke";
+        public MFIdentifier Laundry_SweetpakBroeke = "PD.Tracksuit";
+        public MFIdentifier Laundry_SweetpakTops = "MFiles.Property.SweetpaktopstracksuitTops";
+        public MFIdentifier Laundry_Swembroeke = "PD.Swimsuittrunks";
+        public MFIdentifier Laundry_Tafeldoeke = "MFiles.Property.TafeldoeketableCloth";
+        public MFIdentifier Laundry_THemde = "MFiles.Property.Themdetshirt";
+        public MFIdentifier Laundry_Treklaken = "MFiles.Property.TreklakenfittedSheet";
+        public MFIdentifier Laundry_Truie = "PD.Jersey";
+        public MFIdentifier Laundry_WasgoedSakke = "MFiles.Property.Wasgoedsakke";
+    }
+}
