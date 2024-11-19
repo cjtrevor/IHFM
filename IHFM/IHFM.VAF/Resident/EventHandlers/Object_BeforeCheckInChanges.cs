828dab4e5a980b7b88ce3904f292e77343093c91
RCadmin <RCadmin@T50CLOUD>
2024-11-19 12:01:21 +0200

    Replaced null check with internal check

--- IHFM/IHFM.VAF/Resident/EventHandlers/Object_BeforeCheckInChanges.cs
+++ IHFM/IHFM.VAF/Resident/EventHandlers/Object_BeforeCheckInChanges.cs
@@ -24,11 +24,10 @@ namespace IHFM.VAF
                 }          
                 
                 if (change.PropertyDef == Configuration.CurrentRoom.ID && change.ChangeType == PropertyValueChangeType.Modified && env.ObjVerEx.HasValue(Configuration.CurrentRoom))
-                {
-                    ObjVerEx oldRoom = new ObjVerEx(env.Vault, change.OldValue.TypedValue.GetValueAsLookup());
-
-                    if(oldRoom != null)
-                    { 
+                {                   
+                    if(!change.OldValue.TypedValue.IsNULL())
+                    {
+                        ObjVerEx oldRoom = new ObjVerEx(env.Vault, change.OldValue.TypedValue.GetValueAsLookup());
                         SetRoomVacancy(oldRoom, env.Vault, true); //SetRoomVacancy old room vacant
                     }
 
