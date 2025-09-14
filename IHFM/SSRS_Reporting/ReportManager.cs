1ecc68219485cae8a1881520d9fec9144928d45a
psuche <singhpranesh3@gmail.com>
2025-09-14 21:58:14 +0200

    created ssrs report project

---
+++ IHFM/SSRS_Reporting/ReportManager.cs
@@ -0,0 +1,49 @@
+﻿using SSRS_Reporting.SSRS_Reports;
+using System.Net;
+
+namespace SSRS_Reporting
+{
+    public class ReportManager
+    {
+        private readonly ReportExecutionService _reportServerExecutionService;
+
+        public ReportManager
+            (
+            string reportServerWsdlUrl,
+            string username,
+            string password,
+            string domain)
+        {
+            _reportServerExecutionService = new ReportExecutionService
+            {
+                Url = reportServerWsdlUrl,
+                Credentials = new NetworkCredential(username, password, domain)
+            };
+        }
+
+        public byte[] Render
+            (
+            string reportDirectory,
+            string reportName,
+            string reportFormat,
+            ParameterValue[] parameters
+            )
+        {
+            _reportServerExecutionService.ExecutionHeaderValue = new ExecutionHeader();
+            ExecutionInfo execInfo = _reportServerExecutionService.LoadReport($"/{reportDirectory}/{reportName}", null);
+            _reportServerExecutionService.SetExecutionParameters(parameters, "en-us");
+
+            string encoding;
+            string mimeType;
+            string extension;
+            Warning[] warnings;
+            string[] streamIds;
+
+            var result = _reportServerExecutionService.Render(reportFormat, null, out extension, out encoding, out mimeType, out warnings, out streamIds);
+
+            return result;
+        }
+
+
+    }
+}
---
+++ IHFM/SSRS_Reporting/Services/BaseReportService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSRS_Reporting.Services
+{
+    public class BaseReportService
+    {
+        protected ReportManager reportManager;
+
+        public BaseReportService()
+        {
+            reportManager = new ReportManager
+                (
+                    "http://singh/ReportServer/ReportExecution2005.asmx",
+                    "username",
+                    "password",
+                    "domain"
+                );
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
