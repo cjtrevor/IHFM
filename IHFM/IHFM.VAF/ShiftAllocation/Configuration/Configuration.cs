00ca91646a27aeda6ce71fb9bc542e31fd3ddf75
psuche <singhpranesh3@gmail.com>
2025-11-04 10:14:13 +0200

    Shift Allocation Service

---
+++ IHFM/IHFM.VAF/ShiftAllocation/Configuration/Configuration.cs
@@ -0,0 +1,24 @@
+﻿using MFiles.VAF.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IHFM.VAF
+{
+    public partial class Configuration
+    {
+        [MFPropertyDef]
+        public MFIdentifier ShiftAllocation_Resident = "MFiles.Property.ResidentHbc";
+
+        [MFPropertyDef]
+        public MFIdentifier ShiftAllocation_StartDate = "MFiles.Property.Date";
+
+        [MFPropertyDef]
+        public MFIdentifier ShiftAllocation_Time = "MFiles.Property.StartTime";
+
+        [MFPropertyDef]
+        public MFIdentifier ShiftAllocation_EndDate = "MFiles.Property.EndDateText";
+    }
+}
---
+++ IHFM/IHFM.VAF/ShiftAllocation/EventHandlers/Object_BeforeCreateNewObjectFinalize.cs
@@ -0,0 +1,93 @@
+﻿using MFiles.VAF.Common;
+using MFiles.VAF.Configuration;
+using MFilesAPI;
+using System;
+using System.Collections.Generic;
+
+namespace IHFM.VAF
+{
+    public partial class VaultApplication
+    {
+        [EventHandler(MFEventHandlerType.MFEventHandlerBeforeCreateNewObjectFinalize, Class = "MFiles.Class.ShiftAllocation")]
+        public void BeforeCreateNewShiftAllocation(EventHandlerEnvironment env)
+        {
+            var shiftAllocationDatePart = env.ObjVerEx.GetPropertyText(Configuration.ShiftAllocation_StartDate);
+            var shiftAllocationTimePart = env.ObjVerEx.GetPropertyText(Configuration.ShiftAllocation_Time);
+            DateTime selectedDate = DateTime.Parse($"{shiftAllocationDatePart} {shiftAllocationTimePart}");
+
+            Lookup residentLookup = env.ObjVerEx.GetProperty(Configuration.ResidentLookup).TypedValue.GetValueAsLookup();
+            ObjVerEx resident = new ObjVerEx(env.Vault, residentLookup);
+            
+            CarePlanSearchService searchService = new CarePlanSearchService(env.Vault, Configuration);
+            ObjVerEx careplan = searchService.GetResidentCarePlanExisting(residentLookup.Item);
+
+            List<ObjVer> objVers = new List<ObjVer>();
+
+            objVers.AddRange(GetTBCSItemsByResident(resident, Configuration.DailyADLLookup));
+            objVers.AddRange(GetTBCSItemsByResident(resident, Configuration.WednesdayADLLookup));
+            objVers.AddRange(GetTBCSItemsByResident(resident, GetADLAliasForDayOfWeek(selectedDate)));
+
+            int totalTimeInMinutes = 0;
+
+            foreach (ObjVer item in objVers)
+            {
+                var objVerEx = new ObjVerEx(env.Vault, item);
+
+                var timeBasedCareItemLookup = objVerEx.GetProperty(Configuration.TBCS_TimeBasedCareItem).TypedValue.GetValueAsLookup();
+                ObjVer timeBasedCareItem = timeBasedCareItemLookup.GetAsObjVer();
+                var timeBasedCareItemVerEx = new ObjVerEx(env.Vault, timeBasedCareItem);
+
+                Int32.TryParse(timeBasedCareItemVerEx.GetPropertyText(Configuration.AverageTime), out int time);
+                totalTimeInMinutes += time;
+            }
+
+            var calculatedEndDate = selectedDate.AddMinutes(totalTimeInMinutes);
+            env.ObjVerEx.SetProperty(Configuration.ShiftAllocation_EndDate, MFDataType.MFDatatypeText, calculatedEndDate.ToString());
+
+            env.ObjVerEx.SaveProperties();
+        }
+
+
+
+        private List<ObjVer> GetTBCSItemsByResident(ObjVerEx resident, MFIdentifier alias)
+        {
+            List<ObjVer> objVers = new List<ObjVer>();
+
+            Lookups tbcScheduleItems = resident.GetLookups(alias);
+
+            foreach (Lookup item in tbcScheduleItems)
+            {
+                objVers.Add(item.GetAsObjVer());
+            }
+
+            return objVers;
+        }
+
+        private MFIdentifier GetADLAliasForDayOfWeek(DateTime dateToCheck)
+        {
+
+            var dayOfWeek = dateToCheck.DayOfWeek;
+
+            switch (dateToCheck.DayOfWeek)
+            {
+                case DayOfWeek.Sunday:
+                    return Configuration.SundayADLLookup;
+                case DayOfWeek.Monday:
+                    return Configuration.MondayADLLookup;
+                case DayOfWeek.Tuesday:
+                    return Configuration.TuesdayADLLookup;
+                case DayOfWeek.Wednesday:
+                    return Configuration.WednesdayADLLookup;
+                case DayOfWeek.Thursday:
+                    return Configuration.ThursdayADLLookup;
+                case DayOfWeek.Friday:
+                    return Configuration.FridayADLLookup;
+                case DayOfWeek.Saturday:
+                    return Configuration.SaturdayADLLookup;
+                default:
+                    return Configuration.SundayADLLookup;
+            }
+        }
+
+    }
+}
