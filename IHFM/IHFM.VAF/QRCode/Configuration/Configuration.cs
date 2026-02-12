c5eca722df1f53a5a0b97258277daa84e6661c67
psuche <singhpranesh3@gmail.com>
2026-02-12 22:55:42 +0200

    QR Code Generation working sample - Refinement needed

---
+++ IHFM/IHFM.VAF/QRCode/Configuration/Configuration.cs
@@ -0,0 +1,41 @@
+﻿using MFiles.VAF.Configuration;
+
+namespace IHFM.VAF
+{
+    public partial class Configuration
+    {
+        [MFPropertyDef]
+        public MFIdentifier QRCode_Resident = "MFiles.Property.Resident";
+
+        [MFPropertyDef]
+        public MFIdentifier QRCode_RoomNumber_Text = "MFiles.Property.RoomNumber"; //NO ALIAS
+
+        [MFPropertyDef]
+        public MFIdentifier QRCode_Room_List = "MFiles.Property.RoomList"; //NO ALIAS //BOTTOM ONE IN SELECT VALUE LIST DROPDOWN
+
+
+
+
+        ////////////////////////////////////////////////////////////////////////////////////////////
+        ////////////////////////////////      SELF MADE PROPS      ////////////////////////////////
+        //////////////////////////////////////////////////////////////////////////////////////////
+
+        [MFPropertyDef]
+        public MFIdentifier QRCode_ResRoom_SelfMade = "MFiles.Property.Resroom"; //CHOOSE FROM LIST "ROOMS" //TOP ONE IN SELECT VALUE LIST DROPDOWN
+        [MFPropertyDef]
+        public MFIdentifier QRCode_QRClass = "MFiles.Property.QrClass"; //REQUIRED
+
+        //Objects
+        [MFObjType]
+        public MFIdentifier QRCode_DailyCareObject = "MFiles.Object.DailyCare";
+
+        //Classes
+        [MFClass]
+        public MFIdentifier QRCode_DailyCareObject_HourlyRoundsClass = "MFiles.ClassHourlyRounds";
+
+        [MFValueListItem(ValueList = "MFiles.Valuelist.QrClass")]
+        public MFIdentifier QRCode_QRClasses_HourlyRounds = "{299AFF25-E7FC-45BC-81A7-C1CC6280F6A5}";
+
+
+    }
+}
---
+++ IHFM/IHFM.VAF/QRCode/Eventhandlers/Object_BeforeCheckInChangesFinalize.cs
@@ -0,0 +1,60 @@
+﻿using IHFM.VAF.QRCode.Services;
+using MFiles.VAF.Common;
+using MFilesAPI;
+using SSRS_Reporting.Services;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace IHFM.VAF
+{
+    public partial class VaultApplication
+    {
+        [EventHandler(MFEventHandlerType.MFEventHandlerBeforeCheckInChangesFinalize, Class = "MFiles.Class.QRCode")]
+        public void BeforeQRCode_CheckinChangesFinalize(EventHandlerEnvironment env)
+        {
+            try
+            {
+                QRCodeGenerationService qrCodeGenerationService = new QRCodeGenerationService();
+
+                var residentLookupId = env.ObjVerEx.GetLookupID(Configuration.QRCode_Resident);
+                var roomNumber = env.ObjVerEx.GetPropertyText(Configuration.QRCode_RoomNumber_Text);
+                var resRoom = env.ObjVerEx.GetLookupID(Configuration.QRCode_ResRoom_SelfMade);
+                var roomList = env.ObjVerEx.GetLookupID(Configuration.QRCode_Room_List);
+
+                string mfilesVaultGuid = Guid.Parse(env.Vault.GetGUID()).ToString("D");
+                var qrClassId = env.ObjVerEx.GetLookupID(Configuration.QRCode_QRClass);
+
+                int classId = -1;
+                switch (qrClassId)
+                {
+                    case 1:
+                        classId = Configuration.QRCode_DailyCareObject_HourlyRoundsClass.ID;
+                        break;
+                }
+
+                Dictionary<int, string> properties = new Dictionary<int, string>
+                {
+                    { 100, classId.ToString() },
+                    { Configuration.QRCode_Resident.ID, residentLookupId.ToString() },
+                    //{ Configuration.QRCode_RoomNumber_Text.ID, roomNumber },
+                    { Configuration.QRCode_ResRoom_SelfMade.ID, resRoom.ToString() },
+                    { Configuration.QRCode_Room_List.ID, roomList.ToString() }
+                };
+
+                var qrCodeImageBytes = qrCodeGenerationService.GenerateQRCodeImage(mfilesVaultGuid, Configuration.QRCode_DailyCareObject.ID, properties);
+
+                var objectId = env.ObjVerEx.ID;
+                File.WriteAllBytes($"C:\\SSRS Temp Output\\{objectId}.jpg", qrCodeImageBytes);
+                env.Vault.ObjectFileOperations.GetFilesForModificationInEventHandler(env.ObjVer);
+                env.Vault.ObjectFileOperations.AddFile(env.ObjVer, $"QRCode{objectId}-{env.ObjVerEx.Version}", "jpg", $"C:\\SSRS Temp Output\\{objectId}.jpg");
+                File.Delete($"C:\\SSRS Temp Output\\{objectId}.jpg");
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
