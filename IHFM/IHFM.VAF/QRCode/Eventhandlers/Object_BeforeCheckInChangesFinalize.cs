2e9684355d4126fe2bdee8c466f8b95d306edf1e
psuche <singhpranesh3@gmail.com>
2026-02-16 21:47:47 +0200

    QRCoder per class QRCode image generation

--- IHFM/IHFM.VAF/QRCode/Eventhandlers/Object_BeforeCheckInChangesFinalize.cs
+++ IHFM/IHFM.VAF/QRCode/Eventhandlers/Object_BeforeCheckInChangesFinalize.cs
@@ -10,45 +10,74 @@ namespace IHFM.VAF
 {
     public partial class VaultApplication
     {
-        [EventHandler(MFEventHandlerType.MFEventHandlerBeforeCheckInChangesFinalize, Class = "MFiles.Class.QRCode")]
-        public void BeforeQRCode_CheckinChangesFinalize(EventHandlerEnvironment env)
+        //[EventHandler(MFEventHandlerType.MFEventHandlerBeforeCheckInChangesFinalize, Class = "MFiles.Class.QRCode")]
+        //public void BeforeQRCode_CheckinChangesFinalize(EventHandlerEnvironment env)
+        //{
+        //    return;
+        //    try
+        //    {
+        //        QRCodeGenerationService qrCodeGenerationService = new QRCodeGenerationService();
+
+        //        var residentLookupId = env.ObjVerEx.GetLookupID(Configuration.QRCode_Resident);
+        //        var roomList = env.ObjVerEx.GetLookupID(Configuration.QRCode_Room_List);
+
+        //        string mfilesVaultGuid = Guid.Parse(env.Vault.GetGUID()).ToString("D");
+        //        var qrClassId = env.ObjVerEx.GetLookupID(Configuration.QRCode_QRClass);
+
+        //        int classId = -1;
+        //        switch (qrClassId)
+        //        {
+        //            case 1:
+        //                classId = Configuration.QRCode_DailyCareObject_HourlyRoundsClass.ID;
+        //                break;
+        //        }
+
+        //        Dictionary<int, string> properties = new Dictionary<int, string>
+        //        {
+        //            { 100, classId.ToString() },
+        //            { Configuration.QRCode_Resident.ID, residentLookupId.ToString() },
+        //            { Configuration.QRCode_Room_List.ID, roomList.ToString() }
+        //        };
+
+        //        var qrCodeImageBytes = qrCodeGenerationService.GenerateQRCodeImage(mfilesVaultGuid, Configuration.QRCode_DailyCareObject.ID, properties);
+
+        //        var objectId = env.ObjVerEx.ID;
+        //        File.WriteAllBytes($"C:\\SSRS Temp Output\\{objectId}.jpg", qrCodeImageBytes);
+        //        env.Vault.ObjectFileOperations.GetFilesForModificationInEventHandler(env.ObjVer);
+        //        env.Vault.ObjectFileOperations.AddFile(env.ObjVer, $"QRCode{objectId}-{env.ObjVerEx.Version}", "jpg", $"C:\\SSRS Temp Output\\{objectId}.jpg");
+        //        File.Delete($"C:\\SSRS Temp Output\\{objectId}.jpg");
+
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        throw new Exception(ex.Message);
+        //    }
+        //}
+
+        [EventHandler(MFEventHandlerType.MFEventHandlerBeforeCheckInChangesFinalize, Class = "MFiles.Class.QrHourlyRounds")]
+        public void BeforeQRCodeHourlyRounds_CheckinChangesFinalize(EventHandlerEnvironment env)
         {
             try
             {
                 QRCodeGenerationService qrCodeGenerationService = new QRCodeGenerationService();
 
+                var mfilesVaultGuid = Guid.Parse(env.Vault.GetGUID()).ToString("D");
+                var classId = Configuration.QRCode_DailyCareObject_HourlyRoundsClass.ID;
                 var residentLookupId = env.ObjVerEx.GetLookupID(Configuration.QRCode_Resident);
-                var roomNumber = env.ObjVerEx.GetPropertyText(Configuration.QRCode_RoomNumber_Text);
-                var resRoom = env.ObjVerEx.GetLookupID(Configuration.QRCode_ResRoom_SelfMade);
-                var roomList = env.ObjVerEx.GetLookupID(Configuration.QRCode_Room_List);
-
-                string mfilesVaultGuid = Guid.Parse(env.Vault.GetGUID()).ToString("D");
-                var qrClassId = env.ObjVerEx.GetLookupID(Configuration.QRCode_QRClass);
-
-                int classId = -1;
-                switch (qrClassId)
-                {
-                    case 1:
-                        classId = Configuration.QRCode_DailyCareObject_HourlyRoundsClass.ID;
-                        break;
-                }
 
                 Dictionary<int, string> properties = new Dictionary<int, string>
                 {
                     { 100, classId.ToString() },
                     { Configuration.QRCode_Resident.ID, residentLookupId.ToString() },
-                    //{ Configuration.QRCode_RoomNumber_Text.ID, roomNumber },
-                    { Configuration.QRCode_ResRoom_SelfMade.ID, resRoom.ToString() },
-                    { Configuration.QRCode_Room_List.ID, roomList.ToString() }
                 };
 
                 var qrCodeImageBytes = qrCodeGenerationService.GenerateQRCodeImage(mfilesVaultGuid, Configuration.QRCode_DailyCareObject.ID, properties);
 
                 var objectId = env.ObjVerEx.ID;
-                File.WriteAllBytes($"C:\\SSRS Temp Output\\{objectId}.jpg", qrCodeImageBytes);
+                File.WriteAllBytes($"C:\\QRGenerationTempOutput\\{objectId}.jpg", qrCodeImageBytes);
                 env.Vault.ObjectFileOperations.GetFilesForModificationInEventHandler(env.ObjVer);
-                env.Vault.ObjectFileOperations.AddFile(env.ObjVer, $"QRCode{objectId}-{env.ObjVerEx.Version}", "jpg", $"C:\\SSRS Temp Output\\{objectId}.jpg");
-                File.Delete($"C:\\SSRS Temp Output\\{objectId}.jpg");
+                env.Vault.ObjectFileOperations.AddFile(env.ObjVer, $"QRCode_HourlyRounds{objectId}-{env.ObjVerEx.Version}", "jpg", $"C:\\QRGenerationTempOutput\\{objectId}.jpg");
+                File.Delete($"C:\\QRGenerationTempOutput\\{objectId}.jpg");
 
             }
             catch (Exception ex)
--- IHFM/IHFM.VAF/QRCode/Services/QRCodeGenerationService.cs
+++ IHFM/IHFM.VAF/QRCode/Services/QRCodeGenerationService.cs
@@ -27,51 +27,6 @@ namespace IHFM.VAF.QRCode.Services
             var qrCodeBytes = pngQrCode.GetGraphic(25);
 
             return qrCodeBytes;
-
-
-
-            //var qrCodeData2 = qrGenerator.CreateQrCode("https://www.coingecko.com/en/coins/cardano", QRCodeGenerator.ECCLevel.Q);
-            //var qrCodeData3 = qrGenerator.CreateQrCode("This is a test QR code", QRCodeGenerator.ECCLevel.Q);
-            //var qrCodeData4 = qrGenerator.CreateQrCode("This is a test QR code", QRCodeGenerator.ECCLevel.M);
-
-            //var qrCodeBytes = qrCodeData.GetRawData(QRCodeData.Compression.Uncompressed);
-
-            //var testClass1 = new QRCodeHelper
-
-            //PngByteQRCodeHelper.GetQRCode("https://www.coingecko.com/en/coins/cardano");
-
-            //var pngQr = new PngByteQRCode(qrCodeData);
-            //var byteThangs = pngQr.GetGraphic(25);
-
-            //var pngQr2 = new PngByteQRCode(qrCodeData2);
-            //var byteThangs2 = pngQr2.GetGraphic(25);
-
-            //var pngQr3 = new PngByteQRCode(qrCodeData3);
-            //var byteThangs3 = pngQr3.GetGraphic(25);
-
-            //var pngQr4 = new PngByteQRCode(qrCodeData4);
-            //var byteThangs4 = pngQr4.GetGraphic(25);
-
-
-            //var nowTimePart = DateTime.Now.ToString("HH_mm_ss_");
-
-
-            //File.WriteAllBytes($"C:\\Vulpixel\\1_LenRenda\\Dump\\qrGen\\{nowTimePart}byteThangs.jpg", byteThangs);
-            //File.WriteAllBytes($"C:\\Vulpixel\\1_LenRenda\\Dump\\qrGen\\{nowTimePart}byteThangs2.png", byteThangs2);
-            //File.WriteAllBytes($"C:\\Vulpixel\\1_LenRenda\\Dump\\qrGen\\{nowTimePart}byteThangs3.png", byteThangs3);
-            //File.WriteAllBytes($"C:\\Vulpixel\\1_LenRenda\\Dump\\qrGen\\{nowTimePart}byteThangs4.png", byteThangs4);
-
-            //File.WriteAllBytes($"C:\\Vulpixel\\1_LenRenda\\Dump\\{nowTimePart}QRBytesJ.jpg", qrCodeBytes);
-            //File.WriteAllBytes($"C:\\Vulpixel\\1_LenRenda\\Dump\\{nowTimePart}QRBytesB.bmp", qrCodeBytes);
-            //File.WriteAllBytes($"C:\\Vulpixel\\1_LenRenda\\Dump\\{nowTimePart}QRBytesG.gif", qrCodeBytes);
-
-
-
-            //qrCodeData.SaveRawData($"C:\\Vulpixel\\1_LenRenda\\Dump\\{nowTimePart}QRCodeData.png", QRCodeData.Compression.Uncompressed);
-
-            //return byteThangs;
-            //throw new NotImplementedException("This method is a placeholder for the QR code generation logic. Please implement the necessary code to generate the QR code based on the requirements of your application.");
         }
-
     }
 }
