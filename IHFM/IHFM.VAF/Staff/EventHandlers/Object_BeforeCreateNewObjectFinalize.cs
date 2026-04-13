86babd0b3b042abbe10ccc4784f6293f51400f0a
psuche <singhpranesh3@gmail.com>
2026-04-13 19:45:52 +0200

    cherry pick align

--- IHFM/IHFM.VAF/Staff/EventHandlers/Object_BeforeCreateNewObjectFinalize.cs
+++ IHFM/IHFM.VAF/Staff/EventHandlers/Object_BeforeCreateNewObjectFinalize.cs
@@ -10,6 +10,39 @@ namespace IHFM.VAF
 {
     public partial class VaultApplication
     {
+        [EventHandler(MFilesAPI.MFEventHandlerType.MFEventHandlerBeforeCreateNewObjectFinalize, Priority = 100)]
+        public void CheckPinCode(EventHandlerEnvironment env)
+        {
+            var pinVal = env.ObjVerEx.Properties.GetProperty(Configuration.StaffLookup_StaffPinCode);
+
+            if (pinVal == null)
+                return;
+
+            MFSearchBuilder mFSearchBuilder = new MFSearchBuilder(env.ObjVerEx.Vault);
+            mFSearchBuilder.Class(Configuration.Staff);
+            mFSearchBuilder.Property(Configuration.Staff_PinCode, MFDataType.MFDatatypeInteger, int.Parse(pinVal.GetValueAsLocalizedText()));
+            var staffUserObject = mFSearchBuilder.FindOneEx();
+
+            if (staffUserObject == null)
+                throw new Exception($"Incorrect pin");
+
+            env.ObjVerEx.SetProperty(Configuration.CreatedBy, MFDataType.MFDatatypeLookup, staffUserObject.ID);
+        }
+
+        [EventHandler(MFEventHandlerType.MFEventHandlerBeforeCheckInChangesFinalize, Class = "MFiles.Class.Staff")]
+        public void BeforeCheckInChanges_EnsureUniqueStaffPinCode(EventHandlerEnvironment env)
+        {
+            var staffPinCode = env.ObjVerEx.Properties.GetProperty(Configuration.Staff_PinCode);
+
+            MFSearchBuilder mFSearchBuilder = new MFSearchBuilder(env.ObjVerEx.Vault);
+            mFSearchBuilder.Class(Configuration.Staff);
+            mFSearchBuilder.Property(Configuration.Staff_PinCode, MFDataType.MFDatatypeInteger, int.Parse(staffPinCode.GetValueAsLocalizedText()));
+            var existingObjectsWithSamePincode = mFSearchBuilder.FindEx();
+
+            if (existingObjectsWithSamePincode.Count > 1)
+                throw new Exception($"Invalid pin, please try a different pin");
+        }
+
         [EventHandler(MFEventHandlerType.MFEventHandlerBeforeCreateNewObjectFinalize, Class = "MFiles.Class.Staff")]
         public void AutoPopulateStaffPropertiesForMobile(EventHandlerEnvironment env)
         {
@@ -31,10 +64,10 @@ namespace IHFM.VAF
             foreach (var saItem in siteAdministrators)
             {
                 env.ObjVerEx.AddLookup(Configuration.SiteAdministrators, saItem.ToObjID().ID);
-        }
+            }
 
             foreach (var stlItem in siteTeamLeaders)
-        {
+            {
                 env.ObjVerEx.AddLookup(Configuration.SiteTeamLeaders, stlItem.ToObjID().ID);
             }
         }
