4cbbb8e25bfb831607e98f9360448ec0488903e9
psuche <singhpranesh3@gmail.com>
2026-05-07 21:44:56 +0200

    WIP(Disabled) - Resident Documents required logic

--- IHFM/IHFM.VAF/Base/Configuration.cs
+++ IHFM/IHFM.VAF/Base/Configuration.cs
@@ -5,6 +5,17 @@ namespace IHFM.VAF
 {
     [DataContract]
     public partial class Configuration
-    { 
+    {
+        public MFIdentifier devTestClass = "MFiles.Class.Triggertest";
+
+
+        public MFIdentifier devTest_Resident = "MFiles.Property.Resident";
+
+        public MFIdentifier ResDocs_Class = "MFiles.Class.ResidentDocuments";
+        public MFIdentifier ResDocs_DocumentType = "MFiles.Property.Documenttypesreq";
+        public MFIdentifier ResDocs_Resident = "MFiles.Property.Resident";
+
+
+
     }
 }
--- IHFM/IHFM.VAF/Base/VaultApplication.cs
+++ IHFM/IHFM.VAF/Base/VaultApplication.cs
@@ -5,6 +5,7 @@ using MFiles.VAF.Configuration;
 using MFiles.VAF.Core;
 using MFilesAPI;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 
 namespace IHFM.VAF
@@ -16,6 +17,65 @@ namespace IHFM.VAF
     public partial class VaultApplication
         : MFiles.VAF.Extensions.ConfigurableVaultApplicationBase<Configuration>
     {
+
+
+
+        [EventHandler(MFEventHandlerType.MFEventHandlerAfterCheckInChangesFinalize, Class = "MFiles.Class.Triggertest")]
+        public void DevTestTriggeRRRRRoonie(EventHandlerEnvironment env)
+        {
+            return;
+            ObjVerEx obj = env.ObjVerEx;
+
+            Lookup resident = obj.GetProperty(Configuration.devTest_Resident).TypedValue.GetValueAsLookup();
+
+            PropertyDef propDef = env.Vault.PropertyDefOperations.GetPropertyDef(3400);
+
+            ValueListItems items = env.Vault.ValueListItemOperations.GetValueListItems(propDef.ValueList);
+
+            var itemDictionary = new Dictionary<int, string>();
+            foreach (ValueListItem item in items)
+                itemDictionary[item.ID] = item.Name;
+
+            string itemIdAndName = "";
+
+            foreach (ValueListItem item in items)
+            {
+                int itemId = item.ID;
+                string itemName = item.Name;
+                itemIdAndName += $"ID: {itemId}, Name: {itemName}\n";
+            }
+
+            //throw new Exception($"Resident ID: {resident.DisplayID}\n Value List Items:\n {itemIdAndName}");
+
+            MFSearchBuilder search = new MFSearchBuilder(env.Vault);
+            search.Class(Configuration.ResDocs_Class);
+            search.Property(Configuration.ResDocs_Resident, MFDataType.MFDatatypeLookup, resident.Item);
+
+            var results = search.FindEx();
+
+            var existingDocTypes = new HashSet<int>();
+            var missingDocTypes = new HashSet<int>(itemDictionary.Keys);
+
+            foreach (var item in results)
+            {
+                var docType = item.GetProperty(Configuration.ResDocs_DocumentType);
+                var docTypeId = item.GetLookupID(Configuration.ResDocs_DocumentType);
+
+                if (itemDictionary.ContainsKey(docTypeId))
+                    existingDocTypes.Add(docTypeId);
+            }
+
+            missingDocTypes.ExceptWith(existingDocTypes);
+
+            var sds = 1;
+
+            //if (search.FindEx().Count > 1)
+            //    return search.FindOneEx();
+
+            //return null;
+
+        }
+
         protected override void StartApplication()
         {
             try
