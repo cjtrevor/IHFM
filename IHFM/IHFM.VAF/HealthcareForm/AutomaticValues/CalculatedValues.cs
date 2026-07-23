71a381fe8dbaec9f8a8908136afb33eb238a4d3d
psuche <singhpranesh3@gmail.com>
2026-07-23 21:56:04 +0200

    Healthcare Forms score totals

---
+++ IHFM/IHFM.VAF/HealthcareForm/AutomaticValues/CalculatedValues.cs
@@ -0,0 +1,57 @@
+﻿using MFiles.VAF.Common;
+using MFilesAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IHFM.VAF
+{
+    public partial class VaultApplication
+    {
+        [PropertyCustomValue("MFiles.Property.BbsTotalScore", Priority = 100)]
+        public TypedValue SetTotalScoreBBSValue(PropertyEnvironment env)
+        {
+            TypedValue calculated = new TypedValue();
+
+            int sittingToStanding = PropertyParser.ExtractPropertyValue(env.ObjVerEx, Configuration.BBS_SittingToStanding);
+            int standingUnsupported = PropertyParser.ExtractPropertyValue(env.ObjVerEx, Configuration.BBS_StandingUnsupported);
+            int sittingWithBackUnsupportedButFeetSupported = PropertyParser.ExtractPropertyValue(env.ObjVerEx, Configuration.BBS_SittingWithBackUnsupportedButFeetSupported);
+            int standingToSitting = PropertyParser.ExtractPropertyValue(env.ObjVerEx, Configuration.BBS_StandingToSitting);
+            int transfers = PropertyParser.ExtractPropertyValue(env.ObjVerEx, Configuration.BBS_Transfers);
+            int standingUnsupportedWithEyesClose = PropertyParser.ExtractPropertyValue(env.ObjVerEx, Configuration.BBS_StandingUnsupportedWithEyesClose);
+            int standingUnsupportedWithFeetTogether = PropertyParser.ExtractPropertyValue(env.ObjVerEx, Configuration.BBS_StandingUnsupportedWithFeetTogether);
+            int reachingForwardWithOutstretchedArmWhileStanding = PropertyParser.ExtractPropertyValue(env.ObjVerEx, Configuration.BBS_ReachingForwardWithOutstretchedArmWhileStanding);
+            int pickUpObjectFromTheFloorFromStandingPosition = PropertyParser.ExtractPropertyValue(env.ObjVerEx, Configuration.BBS_PickUpObjectFromTheFloorFromStandingPosition);
+            int turningToLookBehindBothShouldersWhileStanding = PropertyParser.ExtractPropertyValue(env.ObjVerEx, Configuration.BBS_TurningToLookBehindBothShouldersWhileStanding);
+            int turn360Degrees = PropertyParser.ExtractPropertyValue(env.ObjVerEx, Configuration.BBS_Turn360Degrees);
+            int placeFootOnStepstoolWhileStandingUnsupported = PropertyParser.ExtractPropertyValue(env.ObjVerEx, Configuration.BBS_PlaceFootOnStepstoolWhileStandingUnsupported);
+            int standingUnsupportedOneFootInFront = PropertyParser.ExtractPropertyValue(env.ObjVerEx, Configuration.BBS_StandingUnsupportedOneFootInFront);
+            int standingOnOneLeg = PropertyParser.ExtractPropertyValue(env.ObjVerEx, Configuration.BBS_StandingOnOneLeg);
+
+            calculated.SetValue(MFDataType.MFDatatypeInteger, sittingToStanding + standingUnsupported + sittingWithBackUnsupportedButFeetSupported + standingToSitting + transfers + standingUnsupportedWithEyesClose + standingUnsupportedWithFeetTogether + reachingForwardWithOutstretchedArmWhileStanding + pickUpObjectFromTheFloorFromStandingPosition + turningToLookBehindBothShouldersWhileStanding + turn360Degrees + placeFootOnStepstoolWhileStandingUnsupported + standingUnsupportedOneFootInFront + standingOnOneLeg);
+
+            return calculated;
+        }
+
+        [PropertyCustomValue("MFiles.Property.TotalFratScore", Priority = 100)]
+        public TypedValue SetTotalScoreFRATValue(PropertyEnvironment env)
+        {
+            TypedValue calculated = new TypedValue();
+
+            int age = PropertyParser.ExtractPropertyValue(env.ObjVerEx, Configuration.FRAT_Age);
+            int fallHistory = PropertyParser.ExtractPropertyValue(env.ObjVerEx, Configuration.FRAT_FallHistory);
+            int eliminationBowelAndUrine = PropertyParser.ExtractPropertyValue(env.ObjVerEx, Configuration.FRAT_EliminationBowelAndUrine);
+            int medicationsInUse = PropertyParser.ExtractPropertyValue(env.ObjVerEx, Configuration.FRAT_MedicationsInUse);
+            int equipmentThatTethersPatient = PropertyParser.ExtractPropertyValue(env.ObjVerEx, Configuration.FRAT_EquipmentThatTethersPatient);
+            int mobility = PropertyParser.ExtractPropertyValue(env.ObjVerEx, Configuration.FRAT_Mobility);
+            int cognition = PropertyParser.ExtractPropertyValue(env.ObjVerEx, Configuration.FRAT_Cognition);
+
+            calculated.SetValue(MFDataType.MFDatatypeInteger, age + fallHistory + eliminationBowelAndUrine + medicationsInUse + equipmentThatTethersPatient + mobility + cognition);
+
+            return calculated;
+        }
+
+    }
+}
---
+++ IHFM/IHFM.VAF/HealthcareForm/Configuration/Configuration.cs
@@ -0,0 +1,57 @@
+﻿using MFiles.VAF.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IHFM.VAF
+{
+    public partial class Configuration
+    {
+        [MFPropertyDef]
+        public MFIdentifier BBS_SittingToStanding = "MFiles.Property.SittingToStanding";
+        [MFPropertyDef]
+        public MFIdentifier BBS_StandingUnsupported = "MFiles.Property.StandingUnsupported";
+        [MFPropertyDef]
+        public MFIdentifier BBS_SittingWithBackUnsupportedButFeetSupported = "MFiles.Property.SittingWithBackUnsupportedButFeetSupported";
+        [MFPropertyDef]
+        public MFIdentifier BBS_StandingToSitting = "MFiles.Property.StandingToSitting";
+        [MFPropertyDef]
+        public MFIdentifier BBS_Transfers = "MFiles.Property.Transfers";
+        [MFPropertyDef]
+        public MFIdentifier BBS_StandingUnsupportedWithEyesClose = "MFiles.Property.StandingUnsupportedWithEyesClose";
+        [MFPropertyDef]
+        public MFIdentifier BBS_StandingUnsupportedWithFeetTogether = "MFiles.Property.StandingUnsupportedWithFeetTogether";
+        [MFPropertyDef]
+        public MFIdentifier BBS_ReachingForwardWithOutstretchedArmWhileStanding = "MFiles.Property.ReachingForwardWithOutstretchedArmWhileStanding";
+        [MFPropertyDef]
+        public MFIdentifier BBS_PickUpObjectFromTheFloorFromStandingPosition = "MFiles.Property.PickUpObjectFromTheFloorFromStandingPosition";
+        [MFPropertyDef]
+        public MFIdentifier BBS_TurningToLookBehindBothShouldersWhileStanding = "MFiles.Property.TurningToLookBehindBothShouldersWhileStanding";
+        [MFPropertyDef]
+        public MFIdentifier BBS_Turn360Degrees = "MFiles.Property.Turn360Degrees";
+        [MFPropertyDef]
+        public MFIdentifier BBS_PlaceFootOnStepstoolWhileStandingUnsupported = "MFiles.Property.PlaceFootOnStepstoolWhileStandingUnsupported";
+        [MFPropertyDef]
+        public MFIdentifier BBS_StandingUnsupportedOneFootInFront = "MFiles.Property.StandingUnsupportedOneFootInFront";
+        [MFPropertyDef]
+        public MFIdentifier BBS_StandingOnOneLeg = "MFiles.Property.StandingOnOneLeg";
+
+
+        [MFPropertyDef]
+        public MFIdentifier FRAT_Age = "MFiles.Property.Agefrat";
+        [MFPropertyDef]
+        public MFIdentifier FRAT_FallHistory = "MFiles.Property.FallHistory";
+        [MFPropertyDef]
+        public MFIdentifier FRAT_EliminationBowelAndUrine = "MFiles.Property.EliminationBowelAndUrine";
+        [MFPropertyDef]
+        public MFIdentifier FRAT_MedicationsInUse = "MFiles.Property.MedicationsInUse";
+        [MFPropertyDef]
+        public MFIdentifier FRAT_EquipmentThatTethersPatient = "MFiles.Property.EquipmentThatTethersPatient";
+        [MFPropertyDef]
+        public MFIdentifier FRAT_Mobility = "MFiles.Property.Mobilityfrat";
+        [MFPropertyDef]
+        public MFIdentifier FRAT_Cognition = "MFiles.Property.Cognitionfrat";
+    }
+}
