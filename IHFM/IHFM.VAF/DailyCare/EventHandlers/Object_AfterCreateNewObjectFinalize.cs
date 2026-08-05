51744ade2241abed5da7e8188082914b0fd04b33
psuche <singhpranesh3@gmail.com>
2026-08-05 22:20:05 +0200

    Combined Care logic for IntakeOutput and IncontinenceCare

--- IHFM/IHFM.VAF/DailyCare/EventHandlers/Object_AfterCreateNewObjectFinalize.cs
+++ IHFM/IHFM.VAF/DailyCare/EventHandlers/Object_AfterCreateNewObjectFinalize.cs
@@ -10,7 +10,10 @@ namespace IHFM.VAF
         public void AfterCreateNewDailyCare(EventHandlerEnvironment env)
         {
             if(env.ObjVerEx.HasValue(Configuration.NappyUsage_NappyChange) && env.ObjVerEx.GetProperty(Configuration.NappyUsage_NappyChange).GetValue<bool>())
-                UpdateNappyStock(env.ObjVerEx, env.Vault);
+            {
+                DailyCareService dailyCareService = new DailyCareService(env.Vault, Configuration);
+                dailyCareService.UpdateNappyStock(env.ObjVerEx);
+            }
         }
 
         [EventHandler(MFEventHandlerType.MFEventHandlerAfterCreateNewObjectFinalize, Class = "MFiles.Class.NappyChange")]
@@ -18,23 +21,24 @@ namespace IHFM.VAF
         {
             if(env.ObjVerEx.HasValue(Configuration.IncontinenceSupplies_IncontinenceProduct))
             {
-                UpdateNappyStock(env.ObjVerEx, env.Vault);
+                DailyCareService dailyCareService = new DailyCareService(env.Vault, Configuration);
+                dailyCareService.UpdateNappyStock(env.ObjVerEx);
             }
             
         }
 
-        private void UpdateNappyStock(ObjVerEx change, Vault vault)
-        {
-            NappyUsageService usageService = new NappyUsageService(vault, Configuration);
-            IncontinenceStockUpdateService stockUpdateService = new IncontinenceStockUpdateService(vault, Configuration);
+        //private void UpdateNappyStock(ObjVerEx change, Vault vault)
+        //{
+        //    NappyUsageService usageService = new NappyUsageService(vault, Configuration);
+        //    IncontinenceStockUpdateService stockUpdateService = new IncontinenceStockUpdateService(vault, Configuration);
 
-            int siteID = change.GetLookupID(Configuration.SiteList);
-            int residentId = change.GetLookupID(Configuration.ResidentLookup);
+        //    int siteID = change.GetLookupID(Configuration.SiteList);
+        //    int residentId = change.GetLookupID(Configuration.ResidentLookup);
 
-            usageService.LogMonthlyNappyUsage(siteID, residentId);
+        //    usageService.LogMonthlyNappyUsage(siteID, residentId);
 
-            int productId = change.GetLookupID(Configuration.IncontinenceSupplies_IncontinenceProduct);
-            stockUpdateService.AdjustIncontinenceStockOnHand(residentId, productId, -1);
-        }
+        //    int productId = change.GetLookupID(Configuration.IncontinenceSupplies_IncontinenceProduct);
+        //    stockUpdateService.AdjustIncontinenceStockOnHand(residentId, productId, -1);
+        //}
     }
 }
